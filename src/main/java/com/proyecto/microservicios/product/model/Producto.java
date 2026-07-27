9a62163b6b1a785c0714d3ee6e47d381dbba3cfc
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2026-07-27 17:10:40 +0200

    Añade atributos de clase Producto

--- src/main/java/com/proyecto/microservicios/product/model/Producto.java
+++ src/main/java/com/proyecto/microservicios/product/model/Producto.java
@@ -1,6 +1,16 @@
 package com.proyecto.microservicios.product.model;
 
-@Document(value = "product")
-public class Producto{
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(value = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Producto{
+    @Id
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
 }

b1a3ab540d5487f469c6016f535002fe28109b3b
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2026-07-30 19:35:28 +0200

    Crea test para comprobar que peticiones POST funcionan

--- src/main/java/com/proyecto/microservicios/product/dto/ProductResponse.java
+++ src/main/java/com/proyecto/microservicios/product/dto/ProductResponse.java
@@ -2,6 +2,6 @@ package com.proyecto.microservicios.product.dto;
 
 import java.math.BigDecimal;
 
-public record ProductResponse(String id, String name, String description, BigDecimal price){
-    
+public record ProductResponse(String id, String nombre, String descripcion, BigDecimal precio){
+
 }
--- src/test/java/com/proyecto/microservicios/product/ProductServiceApplicationTests.java
+++ src/test/java/com/proyecto/microservicios/product/ProductServiceApplicationTests.java
@@ -1,15 +1,58 @@
 package com.proyecto.microservicios.product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.hamcrest.Matchers;
+import io.restassured.RestAssured;
+
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+    "nombre": "Cuchara de oro",
+    "descripcion": "Una cuchara hecha de oro.",
+    "precio": 6.69
+}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("nombre", Matchers.equalTo("Cuchara de oro"))
+				.body("descripcion", Matchers.equalTo("Una cuchara hecha de oro."))
+				.body("precio", Matchers.equalTo(6.69f));
+
 	}
 
 }
